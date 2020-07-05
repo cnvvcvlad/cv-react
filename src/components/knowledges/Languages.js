f9a07b6676c15cd5051305ff377396aaf72ee6dc
cnvvcvlad <cnvvc_vlad@yahoo.fr>
2020-07-05 09:45:22 +0200

    add a new experience

--- src/components/knowledges/Languages.js
+++ src/components/knowledges/Languages.js
@@ -14,7 +14,8 @@ class Languages extends Component {
         frameworks: [
             { id: 1, value: "React", xp: 0.1 },
             { id: 2, value: "Bootstrap", xp: 1 },
-            { id: 1, value: "Symfony", xp: 0.5 }
+            { id: 3, value: "Symfony", xp: 0.5 },
+            { id: 4, value: "SASS", xp: 0.5 }
         ]
     }
     render() {
