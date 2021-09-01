c79578ae98d56b68322e3e10e53263673b461618
cnvvcvlad <cnvvc_vlad@yahoo.fr>
2021-09-01 23:22:50 +0200

    add new experience

--- src/components/knowledges/Experience.js
+++ src/components/knowledges/Experience.js
@@ -5,20 +5,24 @@ const Experience = () => {
         <div className="experience">
             <h3>Expérience</h3>
             <div className="exp-1">
+                <h4>Développeur web PHP Symfony</h4>
+                <h5>2021</h5>
+                <p>Durant ma formation chez <a href="https://simplon.co/" target="_blank" rel="noopener noreferrer"><i>Simplon</i></a>, j’ai développé plusieurs applications web coté BackEnd en PHP, avec le framework Symfony.</p>
+            </div>
+            <div className="exp-2">
                 <h4>Stagiaire développement web</h4>
                 <h5>2019</h5>
-                <p>C’est dans le cadre de ma formation à l’AFPA Creteil et durant  la période de stage, j’ai développé plusieurs applications web, qui sont présentées dans le portfolio.</p>
+                <p>C’est dans le cadre de ma formation à l’<a href="https://www.afpa.fr/centre/centre-de-creteil" target="_blank" rel="noopener noreferrer"><i>AFPA Creteil</i></a>  et durant la période de stage, j’ai développé plusieurs applications web, en applicant mes compétences en HTML, CSS, JavaScript et PHP.</p>
             </div>
-
-            <div className="exp-2">
+            <div className="exp-3">
                 <h4>Serveur</h4>
                 <h5>2015-2018</h5>
                 <p>Mon premier job à Paris a été dans la restauration.</p>
             </div>
-            <div className="exp-3">
+            <div className="exp-4">
                 <h4>Consultant télémarketing - <a href="https://www.gpg-callcenter.com/" className="jobLink" target="_blank" rel="noopener noreferrer"> GPGConsulting(Argus de la Presse), Chisinau</a></h4>
                 <h5>2014-2015</h5>
-                <p>C'est un centre de contacts multilingues, qui fourni des services télémarketing, services de développement de logiciels, ainsi que la saisie de données, BPO, KPO, site d'animation et de modération, et de toute sorte de services d'externalisation offshore</p>
+                <p>Fournir des services télémarketing, ainsi que la saisie de données et toute sorte de services d'externalisation offshore</p>
             </div>
         </div>
     );
